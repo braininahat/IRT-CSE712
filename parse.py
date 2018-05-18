c964df13e9dd887c0b3a6ba970a02ce2a5ca6432
Varun Shijo <varunshi@buffalo.edu>
2018-05-18 19:36:15 -0400

    using python-chess

--- parse.py
+++ parse.py
@@ -1,2 +1,18 @@
-import chess
+import chess.pgn
 
+pgn = open("/Users/varunshijo/IRT-CSE712/Top10/anand.pgn")
+games = []
+
+while True:
+    boards = []
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
+    boards.append(board)
+
+    for move in game.main_line():
+        board.push(move)
+        boards.append(board)
+    games.append(boards)
+    print(boards)
+
+print(games)
